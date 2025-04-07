ea0bb459530c7225a2f0d6272613270e75aca6c6
veronica2550 <128062548+veronica2550@users.noreply.github.com>
2025-04-07 15:40:54 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: "assets/.env");
   usePathUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
